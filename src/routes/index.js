4dd327bbfe38ccb487f8f77c72b87984fd24aeeb
Peter John R. Rueca <peterrueca@gmail.com>
2022-10-13 12:10:44 +0800

    wip: Swagger UI for OpenAPI

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
+import path from 'path'
 import { Router } from 'express'
+import YAML from 'yamljs'
+import swaggerUi from 'swagger-ui-express'
 import TodoController from '../controllers/TodoController'
 import response from '../middlewares/response'
 import auth from '../middlewares/auth'
@@ -6,13 +9,19 @@ import auth from '../middlewares/auth'
 const router = Router()
 const controller = new TodoController()
 
-router.use('/api', response, auth)
-router.get('/api/todo/page', controller.paginate.bind(controller))
-router.get('/api/todo', controller.getAll.bind(controller))
-router.get('/api/todo/:id', controller.getItem.bind(controller))
-router.post('/api/todo', controller.addTodo.bind(controller))
-router.put('/api/todo/:id', controller.updateTodo.bind(controller))
-router.delete('/api/todo/:id', controller.removeTodo.bind(controller))
-router.delete('/api/todo', controller.reset.bind(controller))
+router.use('/api', response)
+router.get('/api/todo/page', auth, controller.paginate.bind(controller))
+router.get('/api/todo', auth, controller.getAll.bind(controller))
+router.get('/api/todo/:id', auth, controller.getItem.bind(controller))
+router.post('/api/todo', auth, controller.addTodo.bind(controller))
+router.put('/api/todo/:id', auth, controller.updateTodo.bind(controller))
+router.delete('/api/todo/:id', auth, controller.removeTodo.bind(controller))
+router.delete('/api/todo', auth, controller.reset.bind(controller))
+
+const yml = path.join(__dirname, '../../api_docs.yml')
+const docs = YAML.load(yml)
+
+router.use('/api/docs', swaggerUi.serve)
+router.get('/api/docs', swaggerUi.setup(docs))
 
 export default router
